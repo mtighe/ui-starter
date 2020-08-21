1c1a153123bd9ff1043406f205f2100ff90e009c
Michael Tighe <mtighe@auvik.com>
2020-08-21 19:37:01 -0400

    Remove default code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
 import { ThemeProvider } from "emotion-theming";
 import theme from "./theme";
-import "./App.css";
+
+import { Box, Text } from "rebass";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Box>
+        <Text variant="heading" fontSize={3}>
+          UI Starter Kit
+        </Text>
+        <Text>Everything you need to get started?</Text>
+      </Box>
     </ThemeProvider>
   );
 }
