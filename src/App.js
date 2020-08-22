7b3bfcf2506100fc637b21d214ab07d380e3503f
Michael Tighe <mtighe@auvik.com>
2020-08-21 20:00:46 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { ThemeProvider } from "emotion-theming";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import theme from "./theme";
 
-import { Box, Text } from "rebass";
+import { Box, Flex, Text } from "rebass";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box>
-        <Text variant="heading" fontSize={3}>
-          UI Starter Kit
-        </Text>
-        <Text>Everything you need to get started?</Text>
-      </Box>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Flex>
+          <Link to="/">Home</Link>
+          <Link to="login">Login</Link>
+        </Flex>
+        <Switch>
+          <Route path="/login">
+            <Text>Login</Text>
+          </Route>
+          <Route path="/">
+            <Box>
+              <Text variant="heading" fontSize={3}>
+                UI Starter Kit
+              </Text>
+              <Text>Everything you need to get started?</Text>
+            </Box>
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
